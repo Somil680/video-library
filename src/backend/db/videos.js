9f0ae09c71b57bf7f5722738f35b12a6e7c30462
Somil Agrawal <somilagrawal680@gmail.com>
2023-02-21 10:43:28 +0530

    feat update backend data in video.js

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -27,14 +27,195 @@ export const videos = [
   {
     _id: "3sSGmA1ZjD8" ,
     title: "How to get hired as Frontend Developer" ,
-    // creator: ,
+    creator:" Anshika" ,
     description : "How to get hired as Frontend Developer in 2022 ft. @akshaymarch7 | Learn Web Development| Anshika" ,
   },
-  // {
-  //   _id: ,
-  //   title: ,
-  //   creator: ,
-  //   description:,
-  // },
+  {
+    _id: "m050iy5_2ng",
+  title: "talks",
+  charactor: "sunadar pichai",
+  description: "",
+  modal: false,
+  iframeId: "-yLlMk41sro"
+},{
+  _id: "Lb4IcGF5iTQ",
+  title: "talks",
+  charactor: "Mark Zuckerberg",
+  description: "",
+  modal: false,
+  iframeId: "Lb4IcGF5iTQ"
+}
+  ,{
+    
+    _id: "IgKWPdJWuBQ",
+    title: "talks",
+    charactor: "Elon Musk",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "IgKWPdJWuBQ"
+  },
+  {
+    _id: "bXGhtjezJPY",
+    title: "talks",
+    charactor: "Jack Ma",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "bXGhtjezJPY"
+
+  },
+  {
+    _id: "_Hk7nl8LbQA",
+    title: "talks",
+    charactor: "Ratan Tata",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "PJ2wjBt3Xeg",
+  },
+
+  //finance
+  {
+    _id: "4j2emMn7UaI",
+    title: "finance",
+    charactor: "Ranveer Ahulwalia",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "BKTN4C0m6MY",
+  },
+  {
+    _id: "LLdKcFpHgM8",
+    title: "finance",
+    charactor: "CA Rachna",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "LLdKcFpHgM8",
+  },
+  {
+    _id: "LknADkbbekc",
+    title: "finance",
+    charactor: "Sandeep Maheswari",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "LknADkbbekc",
+  },
+  {
+    _id: "pWU3DYvnP0s",
+    title: "finance",
+    charactor: "Anil Lamba",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "pWU3DYvnP0s",
+  },
+  {
+    _id: "E8xFo5lZGDA",
+    title: "finance",
+    charactor: "Dhaval Bathia",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "E8xFo5lZGDA",
+  },
+
+  //crypto
+
+  {
+    _id: "quYJtGRCsew",
+    title: "crypto",
+    charactor: "World's Largest Conference",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "quYJtGRCsew",
+  },
+  {
+    _id: "a5xNyAxwuPQ",
+    title: "crypto",
+    charactor: "Conference",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "a5xNyAxwuPQ",
+  },
+  {
+    _id: "iFAc91miTMY",
+    title: "crypto",
+    charactor: "Himeesh Madaan",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "iFAc91miTMY",
+  },
+  {
+    _id: "WwWMJ1pXDZM",
+    title: "crypto",
+    charactor: "crypto notes",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "WwWMJ1pXDZM",
+  },
+  {
+    _id: "OA2Emtj5Mjw",
+    title: "crypto",
+    charactor: "Nitish Rajput",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "OA2Emtj5Mjw",
+  },
+
+  //Stockmarket
+  {
+    _id: "DlA2jMueIyc",
+    title: "stock",
+    charactor: "Warren Buffet",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "DlA2jMueIyc",
+  },
+  {
+    _id: "dbt-RB18az0",
+    title: "stock",
+    charactor: "Warren Buffet",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "dbt-RB18az0",
+  },
+  
+  {
+    _id: "LfmK_XuGfQ8",
+    title: "stock",
+    charactor: "Raamdeo Agrawal",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "DlA2jMueIyc",
+  },
+  
+  {
+    _id: "ZCFkWDdmXG8",
+    title: "stock",
+    charactor: "Netflix",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "ZCFkWDdmXG8",
+  },
+  {
+    _id: "FyB5inp5r6M",
+    title: "stock",
+    charactor: "Ankur Warikoo",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "FyB5inp5r6M",
+  },
+  {
+    _id: "8CIwSvPHX7g",
+    title: "stock",
+    charactor: "Harshad Mehta",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    modal: false,
+    iframeId: "j1rRdu1AoSQ",
+  },
+  
 ];
 
+
+// {
+//   _id: ,
+//   title: ,
+//   creator: ,
+//   description:,
+// },
