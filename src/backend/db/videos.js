090d66f269d9cd06651e414e39dc86b7b1786c0b
Somil Agrawal <somilagrawal680@gmail.com>
2023-02-21 10:23:50 +0530

    Initial commit

---
+++ src/backend/db/videos.js
@@ -0,0 +1,40 @@
+/**
+ * Videos Database can be ad-ded here.
+ * You can add videos of your wish with different attributes
+ * */
+
+export const videos = [
+  {
+    _id: "Wo5dMEP_BbI",
+    title: "Awesome Video about Coding",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    creator: "Soham Shah",
+  },
+  {
+    _id: "F_Riqjdh2oM",
+    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
+    creator: "Sentdex",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: "vP3TRAsjZss",
+    title:"Front-End vs Back-End " ,
+    creator:"Apna Collage" ,
+    description:"Front End vs Back End - Which one is good for you ?" ,
+  },
+  {
+    _id: "3sSGmA1ZjD8" ,
+    title: "How to get hired as Frontend Developer" ,
+    // creator: ,
+    description : "How to get hired as Frontend Developer in 2022 ft. @akshaymarch7 | Learn Web Development| Anshika" ,
+  },
+  // {
+  //   _id: ,
+  //   title: ,
+  //   creator: ,
+  //   description:,
+  // },
+];
+
