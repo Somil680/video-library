1b2063f8c468127c239dec294c782a5dc5125d31
Somil Agrawal <somilagrawal680@gmail.com>
2023-03-05 12:30:45 +0530

    feat update responsive watchlator.jsx

--- src/Pages/WatchLator/WatchlatorView.jsx
+++ src/Pages/WatchLator/WatchlatorView.jsx
@@ -6,27 +6,29 @@ import { useWatchLator } from '../../context';
 function WatchlatorView({ watchVideos }) {
     const { watchState, watchDispatch } = useWatchLator()
     const {watchlatorItem} = watchState
-        const {_id , title , iframeId , publishedAt , creator } = watchVideos
-    return (<>
-        <div className="watchlator-videos">
-    <div key={_id} className="watchlator-videos-container">
+    const { _id, title, iframeId, publishedAt, creator } = watchVideos
+    
+return (<>
 <Link key={_id} to={`/watch=/${_id}`} className="Link">
-        <div className='video--recommend'>
-            <img src={`https://i.ytimg.com/vi_webp/${iframeId}/mqdefault.webp `} alt="" className='video-image-recommend' />
+<div key={_id} className="watchlator-videos-container">
+            
+            <div>
+                    <img src={`https://i.ytimg.com/vi_webp/${iframeId}/mqdefault.webp `} alt="" className='video-image-recommend' />
+            </div>
+                
+            <div className='watchlatorvideos-detail'>   
             <div className='video-detail-watchlator'>
                     <h4 className='margin0'>{title}</h4>
                     <p className='margin0'>{creator}</p>
                     <p className="margin0">{publishedAt}</p>    
-             
             </div> 
-        </div>    
-        </Link>    
-        <span className='delete'
-         onClick={()=> watchDispatch({type: "REMOVE-TO-WATCH-LATOR" ,payload: _id})}> 
-            <MdDelete className='bars-icon  '/>
-        </span> 
-    </div>
+            <div className='delete'
+                    onClick={()=> watchDispatch({type: "REMOVE-TO-WATCH-LATOR" ,payload: _id})}> <MdDelete className='bars-icon  '/>
+            </div> 
+            </div>    
 </div>
+</Link>    
+
     </>)
 }
  export {WatchlatorView}
