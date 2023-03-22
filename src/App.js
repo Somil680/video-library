d27343bcd6be64cca59351a238d6eca309f6225f
Somil Agrawal <somilagrawal680@gmail.com>
2023-03-22 16:36:43 +0530

    update in app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import "./App.css";
 
 import { Home, Videoshow, WatchLator , Playlist } from "./Pages";
 import { Routes , Route } from "react-router-dom";
-import { useWatchLator  ,usePlaylist} from "./context";
+import { useWatchLator } from "./context";
 import { Empty,  } from "./Components";
 import { Library } from "./Pages/Library/Library";
 
 function App() {
  const { watchState } = useWatchLator()
   const { watchlatorItem } = watchState
-  const { playlistState } = usePlaylist()
-  const { Playlistitem } = playlistState
 
   return (
     <div className="App" >
@@ -20,7 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/watch=/:videoId" element={<Videoshow />} />
         <Route path="/watchlator" element={watchlatorItem.length ===0 ? <Empty/> : <WatchLator /> } />
-        <Route path="/playlist" element={Playlistitem.length === 0 ? <Empty/> :<Playlist />} />
+        <Route path="/playlist" element={<Playlist />} />
         <Route path="/library" element={<Library/>} />
          </Routes>
      
