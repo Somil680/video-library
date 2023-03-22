d5fbac8acc9e49a6f4ac549ac207342d5d987922
Somil Agrawal <somilagrawal680@gmail.com>
2023-03-22 20:37:50 +0530

    refactor/liked/history/empty

--- src/Components/History/Historyvideo.jsx
+++ src/Components/History/Historyvideo.jsx
@@ -1,40 +1,59 @@
-import "./Historyvideo.css"
-import { useHistory } from "../../context"
-import { Link } from "react-router-dom"
+import React from 'react';
 import { HiDotsVertical } from "react-icons/hi";
+import { Link } from "react-router-dom"
+
+import { useHistory } from "../../context"
+
+import "./Historyvideo.scss"
 
 function History() {
 
     const { historyState } = useHistory()
-    const {historyItem} = historyState
-    // console.log(historyItem)
-    
-let ActualhistoryItem = historyItem.map((_, index, originalArray) => {
-  return originalArray[originalArray.length - 1 - index];
-});
-// console.log(ActualhistoryItem);
+    const { historyItem } = historyState
 
-return (<>
-<div className="main-history-body-container">
-    {ActualhistoryItem.map((item) => (
-    <div key={item._id} className="history-video-container">
-        <Link to={`/watch=/${item._id }`} className="Link">
-                <img src={`https://img.youtube.com/vi/${item.iframeId}/maxresdefault.jpg `} alt="" className='history-video-image' />
-        </Link>
-        <div className='history-video-detail'>
-        <span className='content-detail'>
-            <h5 className='margin0'>{item.title.slice(0, 29)}</h5>
-        
-            <p className='margin0 history-font' >{item.creator}</p>
-        </span>
-        <span className='DotsVertical'>
-            <HiDotsVertical className='bars-icon  '/>
-        </span> 
-        </div> 
-   
-    </div>
-))}  
-</div>    
-</>)
+    let ActualhistoryItem = historyItem.map((_, index, originalArray) => {
+        return originalArray[originalArray.length - 1 - index];
+    });
+
+    const styles = {
+        container: "bfy__history__container",
+        historyCard: {
+            container: "bfy__history__historyCard-container",
+            thumbnail: "bfy__history__historyCard-thumbnail",
+            content: "bfy__history__historyCard-content",
+            detail: "bfy__history__historyCard-detail",
+            title: "bfy__history__historyCard-title",
+            text: "bfy__history__historyCard-text",
+            icon: "bfy__history__historyCard-icon",
+        }
+    }
+
+    const getHistoryVideoView = () => (
+        <div className={styles.container}>
+            {ActualhistoryItem.map((item) => (
+                <div key={item._id} className={styles.historyCard.container}>
+                    <Link to={`/watch=/${item._id}`} className="Link">
+                        <img src={`https://img.youtube.com/vi/${item.iframeId}/maxresdefault.jpg `} alt="" className={styles.historyCard.thumbnail} />
+                    </Link>
+                    <div className={styles.historyCard.content}>
+                        <div className={styles.historyCard.detail}>
+                            <h5 className={styles
+                                .historyCard.title}>{item.title}</h5>
+
+                            <p className={styles.historyCard.text}>{item.creator}</p>
+                        </div>
+                        <div >
+                            <HiDotsVertical className={styles.historyCard.icon} />
+                        </div>
+                    </div>
+
+                </div>
+            ))}
+        </div>
+    )
+
+    return (<div>
+        {getHistoryVideoView()}
+    </div>)
 }
-export {History}
+export { History }
--- src/Components/Liked/Likedvideo.jsx
+++ src/Components/Liked/Likedvideo.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
-import { useLike } from '../../context';
-import "./Likedvideo.css"
 import { Link } from "react-router-dom"
 import { HiDotsVertical } from "react-icons/hi";
+
+import { useLike } from '../../context';
+
+import "./Likedvideo.scss"
+
 function Liked() {
+
     const { likeState } = useLike()
-    const {likeItem} = likeState
-return (<>
-    <div className="main-like-body-container">
-    {likeItem.map((item) => (
-    <div key={item._id} className="like-video-container">
-        <Link to={`/watch=/${item._id }`} className="Link">
-                <img src={`https://img.youtube.com/vi/${item.iframeId}/maxresdefault.jpg `} alt="" className='like-video-image' />
-                </Link>
-        <div className='like-video-detail'>
-        <span className='content-detail'>
-            <h5 className='margin0'>{item.title.slice(0,29)}</h5>
-            <p className='margin0 like-font'>{item.creator}</p>
-        </span>
-        <span className='DotsVertical'>
-            <HiDotsVertical className='bars-icon  '/>
-        </span> 
-        </div> 
-        
-    </div>
-))}  
-</div>    
-    </>)
+    const { likeItem } = likeState
+
+    const styles = {
+        container: "bfy__liked__container",
+        likeCard: {
+            container: "bfy__liked__likeCard-container",
+            thumbnail: "bfy__liked__likeCard-thumbnail",
+            content: "bfy__liked__likeCard-content",
+            detail: "bfy__liked__likeCard-detail",
+            title: "bfy__liked__likeCard-title",
+            text: "bfy__liked__likeCard-text",
+            icon:"bfy__liked__likeCard-icon"
+        }
+    }
+
+    const getLikedVideoView = () => (
+        <div className={styles.container}>
+            {likeItem.map((item) => (
+                <div key={item._id} className={styles.likeCard.container}>
+                    <Link to={`/watch=/${item._id}`} className="Link">
+                        <img src={`https://img.youtube.com/vi/${item.iframeId}/maxresdefault.jpg `} alt="" className={styles.likeCard.thumbnail} />
+                    </Link>
+                    <div className={styles.likeCard.content}>
+                        <div className={styles.likeCard.detail}>
+                            <h5 className={styles.likeCard.title}>{item.title}</h5>
+                            <p className={styles.likeCard.text}>{item.creator}</p>
+                        </div>
+                        <div>
+                            <HiDotsVertical className={styles.likeCard.icon} />
+                        </div>
+                    </div>
+
+                </div>
+            ))}
+        </div>
+    )
+
+    return (<div>
+        {getLikedVideoView()}
+    </div>)
 }
-export {Liked}
+export { Liked }
